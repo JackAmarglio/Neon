ab27bbd9109f8576e099e7575bae7b3d5f323189
JackAmarglio <topdevking@gmail.com>
2022-10-19 22:01:06 -0400

    moved backend to other repo

--- server/models/authtoken.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const authTokenSchema = mongoose.Schema({
-    type: { type: String, required: true },
-    token: { type: String, required: true },
-    email: { type: String, required: true }
-});
-
-export const AuthToken = mongoose.model("authToken", authTokenSchema);
