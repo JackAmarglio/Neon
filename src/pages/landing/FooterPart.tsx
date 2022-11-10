2bb656771129fd5884cf10a71caf5d5b81451324
JackAmarglio <topdevking@gmail.com>
2022-11-10 10:49:07 -0500

    update footer part with background image and input style

--- src/pages/landing/FooterPart.tsx
+++ src/pages/landing/FooterPart.tsx
@@ -41,7 +41,7 @@ const FooterPart = () => {
       alert("fill company")
     }
     isValidPhoneNumber(phone) ? 
-    axios.post("https://neon-backend.vercel.app/neon/neon-data", data).then((res) => {
+    axios.post("localhost:5000/neon/neon-data", data).then((res) => {
       if (res.data.success == true) {
         alert("Successfully sent")
         setClientEmail("");
@@ -56,27 +56,27 @@ const FooterPart = () => {
       <div className="relative px-10" id="contact">
         <Container>
           <div className="items-center justify-center flex pt-12">
-            <strong className="text-3xl text-black contact">Contact Us </strong>
+            <strong className="text-3xl text-white contact">Contact Us </strong>
           </div>
           <div className="flex justify-between pt-10 pb-12 landing-footer">
             <div>
-              <p className="text-2xl">Main email</p>
+              <p className="text-2xl text-white">Main email</p>
               <br />
-              <a href="https://neon-languages@neon-l.com" className="text-xl">
+              <a href="https://neon-languages@neon-l.com" className="text-xl text-white">
                 neon-languages@neon-l.com
               </a>
             </div>
             <p>
-              <p className="text-2xl support">Support team email</p>
+              <p className="text-2xl support text-white">Support team email</p>
               <br />
-              <a href="https://teamsupport@neon-l.com" className="text-xl">
+              <a href="https://teamsupport@neon-l.com" className="text-xl text-white">
                 teamsupport@neon-l.com
               </a>
             </p>
             <div>
-              <p className="text-2xl">Interpreter Line</p>
+              <p className="text-2xl text-white">Interpreter Line</p>
               <br />
-              <a href="" className="text-xl">
+              <a href="" className="text-xl text-white">
                 (860)-935-2645
               </a>
             </div>
@@ -84,42 +84,42 @@ const FooterPart = () => {
           <div className="main-footer pb-12 flex justify-between">
             <div className="footer flex md:flex-col">
               <div className="flex flex-col">
-                <p className="text-black">Name</p>
+                <p className="text-white">Name</p>
                 <input
                   value={name}
                   onChange={(e) => setClientName(e.target.value)}
                   required
-                  className="bg-gray-500 w-[full] lg:mb-0 mb-[15px] outline-none text-[#777] text-xl py-[10px] px-4 rounded-full footer-input"
+                  className="border-black text-white bg-black w-[full] lg:mb-0 mb-[15px] outline-none text-xl py-[10px] px-4 rounded-full"
                 ></input>
               </div>
               <div className="flex flex-col">
-                <p className="ml-12 text-black md: ml-0">Company Name</p>
+                <p className="ml-12 text-white md: ml-0">Company Name</p>
                 <input
                   value={company}
                   onChange={(e) => setClientCompany(e.target.value)}
                   required
-                  className="company bg-gray-500 ml-10 w-[full] md:ml-0 lg:mb-0 mb-[15px] outline-none text-[#777] text-xl py-[10px] px-4 rounded-full footer-input"
+                  className="border-black text-white bg-black w-[full] lg:mb-0 mb-[15px] outline-none text-xl py-[10px] px-4 rounded-full"
                 ></input>
               </div>
             </div>
             <div className="footer flex md:flex-col">
               <div className="flex flex-col">
-                <p className="phone ml-12 text-black">Phone Number</p>
+                <p className="phone text-white">Phone Number</p>
                 <PhoneInput
                   placeholder="Enter phone number"
                   value={phone}
                   required
-                  className="phone_input bg-gray-500 ml-10 w-[full] lg:mb-0 mb-[15px] outline-none text-[#777] text-xl py-[10px] px-4 rounded-full footer-input"
+                  className="phone_input border-black text-white bg-black w-[full] lg:mb-0 mb-[15px] outline-none text-xl py-[10px] px-4 rounded-full"
                   onChange={(e:any) => setClientNumber(e)}
                 />
               </div>
               <div className="flex flex-col">
-                <p className="email ml-12 text-black ">Email</p>
+                <p className="email text-white">Email</p>
                 <input
                   value={email}
                   required
                   onChange={(e) => setClientEmail(e.target.value)}
-                  className="email_input bg-gray-500 ml-10 w-[full] lg:mb-0 mb-[15px] outline-none text-[#777] text-xl py-[10px] px-4 rounded-full footer-input"
+                  className="email_input border-black text-white bg-black w-[full] lg:mb-0 mb-[15px] outline-none text-xl py-[10px] px-4 rounded-full"
                 ></input>
               </div>
             </div>
